3cd839c523916b228c84412eb7d3b5ef1c2a5bfc
kenyagreatparty <ryanallanx91@gmail.com>
2024-11-14 17:52:24 +0300

    Update custom.js delay  from 1000 500 to 200 100 from 200 500 to 200 100

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,13 +5,13 @@
   //Hide Loading Box (Preloader)
   function handlePreloader() {
     if ($('.loader-wrap').length) {
-      $('.loader-wrap').delay(1000).fadeOut(500);
+      $('.loader-wrap').delay(200).fadeOut(100);
     }
   }
 
   if ($(".preloader-close").length) {
     $(".preloader-close").on("click", function () {
-      $('.loader-wrap').delay(200).fadeOut(500);
+      $('.loader-wrap').delay(200).fadeOut(100);
     })
   }
 
